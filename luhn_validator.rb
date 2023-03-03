50f4c5ddce0b0e754d0a3ec45d45d89fc1c48ae5
a0716116z <65119444+a0716116z@users.noreply.github.com>
2023-03-03 16:29:14 +0800

    fix rubocop error

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Credit Card Validation
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,16 +8,21 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-    parity =  (nums_a.length - 1) % 2
-    check_bit = nums_a[0..-2].reduce(0) do |final, num|
-      val = (parity + 1) * num
-      single_sum = val / 10 + val % 10
-      parity = (parity + 1) % 2
-      final + single_sum
-    end
-    (10 - check_bit % 10) % 10 == nums_a[-1]
-    # nums_a[0..-2].each_with_index.reduce(0) do |check_bit, (num, index)|
-    # end
+    parity = (nums_a.length - 1) % 2
+    (10 - find_checkbit(nums_a, parity) % 10) % 10 == nums_a[-1]
     # TODO: use the integers in nums_a to validate its last check digit
+  end
+
+  def find_checkbit(nums_a, parity)
+    nums_a[0..-2].reduce(0) do |final, num|
+      single = single_sum(num, parity)
+      parity = (parity + 1) % 2
+      final + single
+    end
+  end
+
+  def single_sum(num, parity)
+    val = (parity + 1) * num
+    val / 10 + val % 10
   end
 end
